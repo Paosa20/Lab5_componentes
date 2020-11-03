b39fd7a82428737f7495e83b55a358a83005b561
Paosa20 <msanchezc1@ucenfotec.ac.cr>
2020-11-02 20:38:15 -0600

    Parte A y B del lab 5

---
+++ boot/boot/src/main/java/com/cenfotec/boot/repository/JournalRepository.java
@@ -0,0 +1,10 @@
+package com.cenfotec.boot.repository;
+
+
+import com.cenfotec.boot.domain.Journal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JournalRepository extends JpaRepository<Journal, Long> {
+
+
+}
